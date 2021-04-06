63222d81ac9be24179924dfc5f5284ed25a00aff
chinmayachinu <chinmayapanigrahi.cp@gmail.com>
2021-04-06 16:21:49 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CookieService } from './service/cookie.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'dependency-injection';
+  constructor(private cookieService : CookieService){}
+
+  cookieString:string= this.cookieService.cookieReady()
 }
